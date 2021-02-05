0512af8c692c1804473f7e82fc2003b6bd8c6475
Ubuntu <ubuntu@sparql-prov-tpch.lxd>
2021-02-05 16:40:56 +0000

    Load datasets in Virtuoso and Fuseki

---
+++ lib/sparql_bench.rb
@@ -0,0 +1,189 @@
+require 'net/ssh'
+require 'csv'
+require 'benchmark'
+
+class Endpoint
+  def initialize(container, timeout = 3000)
+    @container = container
+    @timeout = timeout
+  end
+
+  def container_ip
+    loop do
+      sleep 1
+      ip = `lxc list --columns n4 --format csv | grep #{@container},`.
+             gsub('(eth0)', '').gsub("#{@container},", '').strip
+      return ip unless ip == ''
+    end
+  end
+
+  def start
+    container_start
+    service_start
+  end
+
+  def container_start
+    puts "Starting container #{@container}"
+    system "lxc start #{@container}"
+    puts 'Container started'
+  end
+
+  def stop
+    system "lxc stop #{@container}"
+  end
+
+  def run_query(file)
+    cmd = "curl -s -m #{@timeout} " +
+          "--data-urlencode \"query=$(cat #{file})\" " +
+          "-H \"Accept: text/csv\" #{endpoint_url}"
+     `#{cmd}`
+  end
+
+  def bench_query(file)
+    result = []
+    cmd = "curl -s -o /dev/null -w \"%{http_code}\" -m #{@timeout} " +
+          "--data-urlencode \"query=$(cat #{file})\" " +
+          "-H \"Accept: text/csv\" #{endpoint_url}"
+    time = Benchmark.measure { result << `#{cmd}` }
+    [[time.real, 300].min] + result
+  end
+end
+
+class LXDFusekiEndpoint < Endpoint
+  def initialize(container, timeout = 300)
+    super(container, timeout)
+    @user = 'ubuntu'
+  end
+
+  def name
+    'fuseki'
+  end
+    
+  def endpoint_url
+    "http://#{container_ip}:3030/ds/sparql"
+  end
+  
+  def service_start
+    puts "Starting service"
+    system "lxc exec #{@container} -- su - ubuntu -c " +
+           "'daemonize -E JVM_ARGS=${JVM_ARGS:--Xmx128G} " +
+           "-c /home/ubuntu/apache-jena-fuseki-3.17.0 " +
+           "-o stdout.log -e stderr.log " +
+           "/home/ubuntu/apache-jena-fuseki-3.17.0/fuseki-server " +
+           "--set tdb:unionDefaultGraph=true " +
+           "--loc=/home/ubuntu/tdb /ds'"
+
+    loop do
+      sleep 1
+      output = `lxc exec #{@container} -- netstat -tln | grep ':3030 '`
+      break if output != ''
+    end
+    puts "Service started"
+  end
+end
+
+class LXDVirtuosoEndpoint < Endpoint
+  def initialize(container, timeout = 300)
+    super(container, timeout)
+    @user = 'debian'
+  end
+
+  def name
+    'virtuoso'
+  end
+  
+  def endpoint_url
+    "http://#{container_ip}:8890/sparql/"
+  end
+
+  def service_start
+    puts "Starting service"
+    virtuoso = 'virtuoso-7.2.5.1'
+    system "lxc exec #{@container} -- su - #{@user} -c " +
+           "'cd ~/#{virtuoso}/var/lib/virtuoso/db && " +
+           "~/#{virtuoso}/bin/virtuoso-t'"
+
+    loop do
+      sleep 1
+      output = `lxc exec #{@container} -- netstat -tl | grep ':8890 '`
+      break if output != ''
+    end
+    puts "Service started"
+  end
+end
+
+def watdiv_bench(endpoint, size, template, scheme, mode, times = 5)
+  puts "Starting workload #{[endpoint, size, template, scheme, mode].join('-')}"
+  
+  endpoint.start
+  queries = Dir[File.join('queries', size, template, scheme, mode, '*.sparql')].sort
+
+  queries.each do |query|
+    puts "warming up #{query}"
+    answers = query.gsub('/', '-').sub('.sparql', '.csv').sub('queries-', "answers/#{endpoint.name}-")
+    File.open(answers, 'w') do |file|
+      file.write endpoint.run_query(query)
+    end
+  end
+
+  results = "results/#{endpoint.name}-#{size}-#{template}-#{scheme}-#{mode}.csv"
+  CSV.open(results, 'w') do |csv|
+    csv << %w{engine size template scheme mode query_id repetition time status}
+    (1..times).each do |repetition|
+      queries.each do |query|
+        puts "running query #{query} (repetition #{repetition})"
+        out = endpoint.bench_query(query)
+        query_name = File.basename(query).sub(/.sparql$/, '')
+        csv << [endpoint.name, size, template, scheme, mode, query_name, repetition, out[0], out[1]]
+      end
+    end
+  end
+  
+  endpoint.stop
+end
+
+class TripleProvEndpoint
+  def initialize
+    @home = '/home/ubuntu/sparql-prov-watdiv/tripleprov_demo/release'
+  end
+
+  def name
+    'tripleprov'
+  end
+
+  def bench_query(file)
+    output = `cd #{@home} && ./tripleprov #{file}`
+    {body: output, time: output.lines.last.split.last}
+  end
+end
+
+def tripleprov_bench(size, template, times = 5)
+  endpoint = TripleProvEndpoint.new
+  scheme = 'namedgraphs'
+  mode = 'T'
+  
+  puts "Starting workload #{[endpoint.name, size, template, scheme, mode].join('-')}"
+
+  # tp.bench_query('/home/ubuntu/sparql-prov-watdiv/queries/10M/S2/namedgraphs/T/00.sparql')
+
+  queries = Dir[File.join('queries', size, template, scheme, mode, '*.sparql')].sort
+
+  results = "results/#{endpoint.name}-#{size}-#{template}-#{scheme}-#{mode}.csv"
+  CSV.open(results, 'w') do |csv|
+    csv << %w{engine size template scheme mode query_id repetition time status}
+    queries.each do |query|
+      (1..times).each do |repetition|
+        puts "Running query #{query} (repetition #{repetition})"
+        result = endpoint.bench_query(File.absolute_path query)
+        
+        answers = query.gsub('/', '-').sub('.sparql', '.csv').sub('queries-', "answers/#{endpoint.name}-")
+        File.open(answers, 'w') do |file|        
+          file.puts result[:body]
+        end
+
+        query_name = File.basename(query).sub(/.sparql$/, '')
+        csv << [endpoint.name, size, template, scheme, mode, query_name, repetition, result[:time], 200]
+      end
+    end
+  end
+end
---
+++ rakelib/create_virtuoso_containers.rake
@@ -0,0 +1,129 @@
+
+desc 'Create Virtuoso 7 container'
+named_task 'create_virtuoso_7_container' do
+  container = 'virtuoso7-debian9'
+
+  puts "Lauching container #{conatiner}"
+  launch_debian_container(container, '9')
+
+  system "lxc exec #{container} -- apt install -y libssl-dev build-essential bison flex " +
+         "autotools-dev automake libtool gperf gawk"
+
+  ip = ''
+  loop do
+    sleep 1
+    ip = container_ip(container)
+    break unless ip == ''
+  end
+
+  Net::SSH.start(ip, 'debian') do |ssh|
+    puts 'Getting virtuoso code'
+    ssh.exec!('git clone https://github.com/openlink/virtuoso-opensource.git')
+
+    puts 'Setup Virtuoso'
+    ssh.exec!('export CFLAGS="-O2 -m64" && cd virtuoso-opensource && ' +
+              './autogen.sh && ' +
+              './configure --prefix=/home/debian/virtuoso-7.2.5.1 && ' +
+              'make && make install')
+    ssh.exec!('mkdir dataset')
+    system "lxc file push config/machine_a256/virtuoso.ini #{container}/home/debian/virtuoso-7.2.5.1/var/lib/virtuoso/db/"
+    puts 'Virtuoso compiled and installed'
+  end
+
+  # Publish the container
+  puts 'Create a container image'
+  system "lxc stop #{container}"
+  system "lxc publish #{container} --alias #{container}"
+end
+
+
+def isql(ssh, command)
+  virtuoso = 'virtuoso-7.2.5.1'
+  p command
+  puts command
+  output = ssh.exec!("cd #{virtuoso}/bin && echo \"#{command}\" | ./isql")
+  puts output
+end
+
+def isql_checkpoint(ssh)
+  isql(ssh, 'checkpoint;')
+  isql(ssh, 'commit work;')
+  isql(ssh, 'checkpoint;')
+end
+
+def virtuoso_7_load_dataset(container)
+  virtuoso = 'virtuoso-7.2.5.1'
+
+  ip = ''
+  loop do
+    sleep 1
+    ip = container_ip(container)
+    break unless ip == ''
+  end
+
+  loop do
+    sleep 1
+    output = `lxc exec #{container} -- netstat -tln | grep ':22 '`
+    break if output != ''
+  end
+  
+  Net::SSH.start(ip, 'debian') do |ssh|
+    output = ssh.exec!("cd #{virtuoso}/var/lib/virtuoso/db && ~/#{virtuoso}/bin/virtuoso-t")
+
+    loop do
+      sleep 1
+      output = ssh.exec!('netstat | grep localhost:1111')
+      ready = output.split("\n").map do |line|
+        line.split.last
+      end.reduce(true) do |is_connected, status|
+        is_connected = (is_connected and (status == 'ESTABLISHED'))
+      end
+      break if ready and output != ''
+    end
+
+    isql(ssh, 'SPARQL SELECT (count(*) as ?initial_triples) WHERE { ?s ?p ?o};')
+    isql(ssh, "DB.DBA.VT_BATCH_UPDATE ('DB.DBA.RDF_OBJ', 'ON', NULL);")
+    isql(ssh, 'DELETE FROM DB.DBA.load_list;')
+    isql(ssh, "ld_dir ('/home/debian/dataset', '*', 'http://example.org/tpch');")
+    isql_checkpoint(ssh)
+    isql(ssh, 'rdf_loader_run();')
+    isql_checkpoint(ssh)
+    isql(ssh, "DB.DBA.RDF_OBJ_FT_RULE_ADD (null, null, 'All');")
+    isql(ssh, 'DB.DBA.VT_INC_INDEX_DB_DBA_RDF_OBJ ();')
+    isql(ssh, 'SPARQL SELECT (count(*) as ?loaded_triples) WHERE { ?s ?p ?o };')
+    isql_checkpoint(ssh)
+  end
+end
+
+# %w{10 100}.each do |size|
+#   desc "Create WatDiv #{size}M Virtuoso 7 container for #{scheme[:name]} reification scheme"
+#   named_task "create_watdiv_#{size}M_#{scheme[:name]}_virtuoso_7_container" do
+#     container = "watdiv-#{size}M-#{scheme[:name]}-virtuoso7-debian9"
+#     system "lxc launch virtuoso7-debian9 #{container}"
+#     scheme[:files].each do |file_pair|
+#       system "lxc file push datasets/#{file_pair[0]} #{container}/home/debian/dataset/#{file_pair[1]}"
+#     end
+#     sleep 10
+#     virtuoso_7_load_dataset(container)
+#   end 
+# end
+
+virtuoso_tasks = []
+
+Dir['datasets/ds_ttl_*'].each do |dataset|
+  scale_factor = dataset.gsub('datasets/ds_ttl_', '')
+  container = "tpch-#{scale_factor}-virtuoso7-debian9".gsub('.','d')
+  task_name = "create-tpch-#{scale_factor}-virtuoso7-debian9"
+  desc "Create #{container}"
+  named_task task_name do
+    system "lxc launch virtuoso7-debian9 #{container}"
+    Dir["#{dataset}/*.ttl.gz"].each do |data_file|
+      system "lxc file push #{data_file} #{container}/home/debian/dataset/"
+    end
+    virtuoso_7_load_dataset(container)
+  end
+
+  virtuoso_tasks << task_dependency(task_name)
+end
+
+task :virtuoso_tasks => virtuoso_tasks
